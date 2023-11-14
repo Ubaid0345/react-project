e7d8d877ce67ab5d627cdcf939ffbd12e009a3b4
ubaidurrehman <ubaidurrehman.edu.pk@gmail.com>
2023-11-15 01:12:21 +0500

    working

---
+++ src/Component/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Greet(props) {
+  return <div className='container card p-3 mt-2 register-container text-center'>
+    <h3>{props.name}</h3>
+    <h3>thank you for joining us</h3>
+    <h3>verification sent to {props.email}</h3>
+  </div>
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Greet from './Component/Greet';
+import Register from './Component/Register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Greet /> */}
+    {/* <Register /> */}
   </React.StrictMode>
 );
 
