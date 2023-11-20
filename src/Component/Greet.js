831d6ceb8d4cb4501079cfec5b11fb9e0d072be7
ubaidurrehman <ubaidurrehman.edu.pk@gmail.com>
2023-11-20 21:10:22 +0500

    1 line remmove

--- src/Component/Greet.js
+++ src/Component/Greet.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 export default function Greet(props) {
   return <div className='container card p-3 mt-2 register-container text-center'>
-    console.log(Greet),
     <h3>{props.name}</h3>
     <h3>thank you for joining us</h3>
     <h3>verification sent to {props.email}</h3>
